ec383472b8a4c2a9ed8fe8772053cfedc745aa39
Dylan Turner <dylantdmt@gmail.com>
2020-10-18 16:53:46 -0400

    Started integrating google speech to text api

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Diagnostics;
+using Google.Cloud.Speech.V1;
 
 namespace man_droid {
     class Program {
         static void Main(string[] args) {
-            
+            if(args.Length < 1) {
+                Console.WriteLine(
+                    "This application requires Google's Speech to Text API"
+                );
+                Console.WriteLine(
+                    "Please provide the JSON file with your credentials as"
+                    + " and argument"
+                );
+            } else if(args.Length > 1) {
+                Console.WriteLine("Too many arguments provided!");
+            }
+
+            var credentiaFileName = args[0];
+            Process.Start(
+                "export GOOGLE_APPLICATION_CREDENTIALS=" + credentiaFileName
+            );
+
+            var speech = SpeechClient.Create();
         }
     }
 }
