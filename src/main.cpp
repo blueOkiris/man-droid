165ae3a940cef072fd4920db892be8048a366728
Dylan Turner <dylantdmt@gmail.com>
2020-10-19 23:42:42 -0400

    Made delay shorter

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,13 @@ inline void testServo() {
     servo.start();
     for(int i = 0; i < 31; i++) {
         servo.setAngle(i);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
     }
     for(int i = 30; i >= 0; i--) {
         servo.setAngle(i);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
     }
+    servo.stop();
 }
 
 int main(int argc, char **args) {
