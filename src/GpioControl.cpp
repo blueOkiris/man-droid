43011e5b707d29f6d2fd657ffb8c1138870d45c9
Dylan Turner <dylantdmt@gmail.com>
2020-10-19 23:22:44 -0400

    Reverted back to system command version

--- src/GpioControl.cpp
+++ src/GpioControl.cpp
@@ -83,16 +83,10 @@ void PwmPin::on() {
     const auto pinFolder = _pwmPinFolder(_pin);
 
     // Ex: echo 1 > /sys/class/pwm/pwmchip1/pwm-1:0/enable
-    /*std::stringstream enableCmd;
+    std::stringstream enableCmd;
     enableCmd << "echo 1 > " << pinFolder << "/enable";
     std::cout << enableCmd.str() << std::endl;
-    system(enableCmd.str().c_str());*/
-    std::stringstream enableFileName;
-    enableFileName << pinFolder << "/enable";
-    int fd;
-    fd = open(enableFileName.str().c_str(), O_WRONLY);
-    write(fd, "1", 1);
-    close(fd);
+    system(enableCmd.str().c_str());
 
     _running = true;
 }
