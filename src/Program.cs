21745f95361310b6efc9141ff13fbf8a99d2bfd7
Dylan Turner <dylantdmt@gmail.com>
2020-10-19 21:20:47 -0400

    Implemented basic sysfs pwm control

--- src/Program.cs
+++ src/Program.cs
@@ -22,7 +22,7 @@ namespace mandroid {
                 "export GOOGLE_APPLICATION_CREDENTIALS=" + credentiaFileName
             );
 
-            var speech = SpeechClient.Create();
+            
         }
     }
 }
