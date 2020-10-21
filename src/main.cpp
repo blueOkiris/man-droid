2df7d4fd20f353f2d0a54529f645c39f86df23ed
Dylan Turner <dylantdmt@gmail.com>
2020-10-21 15:49:14 -0400

    Extracted tests to separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,7 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-#include <GpioControl.hpp>
-#include <Servo.hpp>
+#include <Tests.hpp>
 
 using namespace mandroid;
-
-inline void testPwm() {
-    std::cout << "Testing pwm..." << std::endl;
-    auto light = PwmPin(
-        PinName::Pwm_P9_22,
-        std::chrono::nanoseconds(500000000),
-        std::chrono::nanoseconds(1000000000)
-    );
-    light.init();
-    light.on();
-
-    std::string waitStr;
-    std::cout << "Type a short response and press enter to end...";
-    std::cin >> waitStr;
-
-    light.deInit();
-}
-
-inline void testServo() {
-    auto servo = Servo(PinName::Pwm_P9_22);
-    servo.start();
-    for(int i = 0; i < 31; i++) {
-        servo.setAngle(i);
-        std::this_thread::sleep_for(std::chrono::milliseconds(250));
-    }
-    for(int i = 30; i >= 0; i--) {
-        servo.setAngle(i);
-        std::this_thread::sleep_for(std::chrono::milliseconds(250));
-    }
-    servo.stop();
-}
 
 int main(int argc, char **args) {
     std::cout << "Initializing the Mandroid..." << std::endl;
