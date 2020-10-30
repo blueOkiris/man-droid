c553df6bca4b2d5a4fd5db77d98b82802b2cdf71
Dylan Turner <dylantdmt@gmail.com>
2020-10-30 19:20:17 +0000

    Commented out python calling

--- src/Servo.cpp
+++ src/Servo.cpp
@@ -1,6 +1,6 @@
 #include <chrono>
 #include <string>
-#include <pybind11/embed.h>
+//#include <pybind11/embed.h>
 #include <Servo.hpp>
 
 using namespace mandroid;
@@ -10,22 +10,22 @@ Servo::Servo(PinName pin) :
 }
 
 void Servo::start() const {
-    pybind11::scoped_interpreter guard{};
+    /*pybind11::scoped_interpreter guard{};
     auto pwm = pybind11::module::import("Adafruit_BBIO.PWM");
-    pwm.attr("start")(pybind11::str(_pinName.c_str()), 97, 60);
+    pwm.attr("start")(pybind11::str(_pinName.c_str()), 97, 60);*/
 }
 
 void Servo::setAngle(const int &angle) const {
-    pybind11::scoped_interpreter guard{};
+    /*pybind11::scoped_interpreter guard{};
     auto pwm = pybind11::module::import("Adafruit_BBIO.PWM");
     pwm.attr("set_duty_cycle")(
         pybind11::str(_pinName.c_str()), dutyFromAngle(angle)
-    );
+    );*/
 }
 
 void Servo::stop() const {
-    pybind11::scoped_interpreter guard{};
+    /*pybind11::scoped_interpreter guard{};
     auto pwm = pybind11::module::import("Adafruit_BBIO.PWM");
     pwm.attr("stop")(pybind11::str(_pinName.c_str()));
-    pwm.attr("cleanup")();
+    pwm.attr("cleanup")();*/
 }
